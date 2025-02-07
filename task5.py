4d869ef5266dd323cdd9578ef0bf0fed581d842d
Xlxlrdyyy <solo.vlad55555@icloud.com>
2025-02-07 06:22:33 +0000

    сделал

---
+++ task5.py
@@ -0,0 +1,9 @@
+# Часть а)
+print("Часть а)")
+for i in range(21, 9, -1):
+    print(f"{i} {i - 1.8}")
+
+# Часть б)
+print("\nЧасть б)")
+for i in range(45, 24, -1):
+    print(f"{i} {i - 0.5} {i - 0.8}")
