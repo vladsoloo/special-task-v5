9c1417481591e273f171c61fda88dda6f206e651
Xlxlrdyyy <solo.vlad55555@icloud.com>
2025-02-07 08:05:14 +0000

    сделал

---
+++ task42.py
@@ -0,0 +1,10 @@
+exam1 = float(input("Введите оценку за первый экзамен: "))
+exam2 = float(input("Введите оценку за второй экзамен: "))
+exam3 = float(input("Введите оценку за третий экзамен: "))
+exam4 = float(input("Введите оценку за четвертый экзамен: "))
+
+
+total_score = exam1 + exam2 + exam3 + exam4
+
+
+print("Сумма набранных баллов:", total_score)
