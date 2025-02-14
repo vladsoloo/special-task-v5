a04ab2b5ccc22c6c7962357156b4b80b95c52f3e
Xlxlrdyyy <solo.vlad55555@icloud.com>
2025-02-14 06:08:55 +0000

    сделал

---
+++ task53.py
@@ -0,0 +1,5 @@
+def student_average(grades):
+    return sum(grades) / len(grades)
+
+student_grades = [8, 9, 10, 7, 8, 9, 8, 9, 10, 9]
+print("Средняя оценка ученика:", student_average(student_grades))
