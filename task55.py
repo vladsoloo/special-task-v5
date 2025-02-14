31fd3f97c9f8200d05bcc60858792f343a13d084
Xlxlrdyyy <solo.vlad55555@icloud.com>
2025-02-14 06:12:09 +0000

    сделал

--- task55.py
+++ task55.py
@@ -1,5 +1,6 @@
 def average_mass(masses):
     return sum(masses) / len(masses)
 
+
 items = [5, 10, 15, 20, 25]
 print("Средняя масса предметов:", average_mass(items))
--- task57.py
+++ task57.py
@@ -4,6 +4,7 @@ def total_scores(first_student, second_student):
     print("Сумма оценок первого ученика:", first_total)
     print("Сумма оценок второго ученика:", second_total)
 
+
 first_student = [8, 9, 10, 7, 8]
 second_student = [9, 8, 9, 8, 9]
 total_scores(first_student, second_student)
